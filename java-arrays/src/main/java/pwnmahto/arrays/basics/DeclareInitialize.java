2d620286c15291e57fe50083d5a36238a0add685
Pawan Kumar <35704365+pwnmahto@users.noreply.github.com>
2020-04-21 13:49:18 +0530

    Code refactoring.

--- java-arrays/src/main/java/pwnmahto/arrays/basics/DeclareInitialize.java
+++ java-arrays/src/main/java/pwnmahto/arrays/basics/DeclareInitialize.java
@@ -21,7 +21,10 @@ public class DeclareInitialize {
 		for (int i = 0; i < 5; i++) {
 			System.out.println(x[i]);
 		}
-		
-		int y[]={1,2,3,4,5,6,7,8,9};//Array can be declared, instantiated and initialized together. 
+
+		int y[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };// Array can be declared, instantiated and initialized together.
+		for (int temp : y) {
+			System.out.println(temp);
+		}
 	}
 }
