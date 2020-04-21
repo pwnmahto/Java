fb133316a00985197da6641bbd338990325bc3b1
Pawan Kumar <35704365+pwnmahto@users.noreply.github.com>
2020-04-21 13:31:54 +0530

    Java Arrays Basic Structure.

---
+++ java-arrays/src/main/java/pwnmahto/arrays/App.java
@@ -0,0 +1,9 @@
+package pwnmahto.arrays;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ java-arrays/src/main/java/pwnmahto/arrays/basics/DeclareInitialize.java
@@ -0,0 +1,27 @@
+package pwnmahto.arrays.basics;
+
+/**
+ * 
+ * A program to demonstrate how to declare, instantiate, and initialize an array
+ * in Java.
+ * 
+ */
+
+public class DeclareInitialize {
+
+	public static void main(String[] args) {
+
+		int x[] = new int[5]; // declaration and instantiation, also instantiation can be done separately
+		x[0] = 1; // initialization
+		x[1] = 2;
+		x[2] = 3;
+		x[3] = 4;
+		x[4] = 5;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(x[i]);
+		}
+		
+		int y[]={1,2,3,4,5,6,7,8,9};//Array can be declared, instantiated and initialized together. 
+	}
+}
